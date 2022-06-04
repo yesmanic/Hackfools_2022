2480a6f94c84dbec4425d1ed3584b0b2d822ead4
Yesman <62268626+yesmanic@users.noreply.github.com>
2022-06-04 18:03:51 -0300

    Update SkyBurger.js

--- SkyBurger.js
+++ SkyBurger.js
@@ -116,7 +116,7 @@ function handleToppings() {
     if (keyIsDown(ENTER) || score === 10) {
         endGame();
         setTimeout(() => {
-            window.location.replace("Hackfools_2022/pedido.html")
+            window.location.replace("pedido.html")
         }, 2000)
     }
 
